576de8ea8d2cba326f68ec0704bd8fd9f23bcf4c
Noah Fajarda <noahfajarda@gmail.com>
2024-01-19 17:03:14 -0800

    added error handling for data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -14,15 +14,21 @@ export interface event {
 
 export default function Home() {
   const [events, setEvents] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     getData();
   }, []);
 
   const getData = async () => {
-    const res = await fetch("data/events.json");
-    const data = await res.json();
-    setEvents(data);
+    try {
+      const res = await fetch("data/events.json");
+      const data = await res.json();
+      setEvents(data);
+    } catch (err) {
+      console.log(err);
+      setError(true);
+    }
   };
 
   return (
@@ -32,10 +38,16 @@ export default function Home() {
           Bob's Orchestra Hall
         </h1>
       </div>
+
+      <div id="section-title" className="text-center py-3">
+        <h2 className="text-3xl font-serif underline">Schedule</h2>
+        <p className="italic">All Times Reflect Seattle, WA Timezone (PST)</p>
+      </div>
+
       <div className="flex justify-center p-5">
         <div className="data-container flex flex-col gap-4">
-          {events.length > 0 ? (
-            <div className="flex flex-col gap-2">
+          {events.length > 0 && !error ? (
+            <div id="events-container" className="flex flex-col gap-2">
               {/* iterate through all events */}
               {events.map((event: event) => {
                 return (
@@ -50,8 +62,13 @@ export default function Home() {
                 );
               })}
             </div>
-          ) : (
+          ) : !error ? (
             <div className="font-serif text-3xl">Loading...</div>
+          ) : (
+            // error handling
+            <div className="font-serif text-red-400 text-2xl">
+              Sorry, There Was An Error On Our End.
+            </div>
           )}
         </div>
       </div>
